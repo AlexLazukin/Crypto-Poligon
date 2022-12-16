18a41cd5b4b42ce4442820ef37183395a18ceb22
Alexey Lazukin <120718910+AlexLazukin@users.noreply.github.com>
2022-12-16 13:25:07 +0300

    App | Empty app created

---
+++ Crypto Poligon/Crypto Poligon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto Poligon
+//
+//  Created by Alexey Lazukin on 16.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
